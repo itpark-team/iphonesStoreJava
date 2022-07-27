a24196ddabac3d1c899046018a3d0bc465ef2e7a
itpark <a.m.smyslov@yandex.ru>
2022-07-27 23:03:07 +0300

    made simple

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,29 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        PhonesManager phonesManager = new PhonesManager();
-        
+        //PhonesManager phonesManager = new PhonesManager();
+
+        ArrayList<Phone> phones = new ArrayList<>();
+
 
         String model = Util.inputString("Введите модель телефона: ");
         int price = Util.inputInt("Введите цену телефона:", 1, 1000000);
         int quantity = Util.inputInt("Введите кол-во добавляемых телефонов: ", 1, 500);
         Phone phone = new Phone(model, price, quantity);
-        phonesManager.addPhone(phone);
+        //phonesManager.addPhone(phone);
 
+        phones.add(phone);
 
-        for (int i = 0; i < phonesManager.getSize(); i++) {
-            System.out.println(phonesManager.getPhone(i));
+//        for (int i = 0; i < phonesManager.getSize(); i++) {
+//            System.out.println(phonesManager.getPhone(i));
+//        }
+
+        for (int i = 0; i < phones.size(); i++) {
+            System.out.println(phones.get(i));
         }
     }
 }
